7d58a440c7afadee487375b3239f49556b867df3
realTronsi <ks.yan@yahoo.com>
2022-11-06 17:56:20 -0500

    Base pose from vision implementation

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,8 +4,14 @@
 
 package frc.robot.subsystems;
 
-import org.photonvision.PhotonCamera;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
@@ -14,6 +20,23 @@ public class Vision extends SubsystemBase {
   /** Creates a new Vision. */
   public Vision(String name) {
     camera = new PhotonCamera(name);
+  }
+
+  public Pose2d getPoseFromVision() {
+    // temporary
+    Map<Integer, Transform3d> lookup = new HashMap<>();
+
+    if(!camera.getLatestResult().hasTargets()) return null;
+    PhotonTrackedTarget target = camera.getLatestResult().getBestTarget();
+    int id = target.getFiducialId();
+
+    // Currently assumes camera at center of robot
+    Transform3d transform = lookup.get(id).plus(target.getBestCameraToTarget().inverse());
+
+    return new Pose2d(
+      transform.getTranslation().toTranslation2d(),
+      transform.getRotation().toRotation2d()
+    );
   }
 
   @Override
