95cddf9c5ce62af5fad813b553a1a1eedac7ecc5
realTronsi <ks.yan@yahoo.com>
2022-11-07 12:51:28 -0500

    Remove unused import

--- src/main/java/frc/robot/Superstructure.java
+++ src/main/java/frc/robot/Superstructure.java
@@ -1,7 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.math.MatBuilder;
-import edu.wpi.first.math.Nat;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
