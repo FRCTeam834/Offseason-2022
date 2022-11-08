01b597ed0ce1dd1cc5d3fa1a3b04041fc1c1caf8
realTronsi <ks.yan@yahoo.com>
2022-11-07 22:47:24 -0500

    Update getPoseFromVision implementation

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -66,31 +66,39 @@ public class Vision extends SubsystemBase {
     Map<Integer, Pose3d> AprilTagLookup = new HashMap<>();
 
     // Currently Simple filtering strategy
-    // Removes poses with high pose ambiguity
+    // Simply gets the first "best" tag with low enough pose ambiguity
     PhotonPipelineResult latestResult = camera.getLatestResult();
     if(!latestResult.hasTargets()) return null;
 
     List<PhotonTrackedTarget> allTargets = latestResult.getTargets();
 
-    PhotonTrackedTarget bestTarget = null;
+    Pose3d bestPose = null;
 
     for (PhotonTrackedTarget target : allTargets) {
       if (target.getPoseAmbiguity() > 0.2) continue;
 
-      bestTarget = target;
+      // Get Pose3d of apriltag
+      int tagID = target.getFiducialId();
+
+      Pose3d tagPose = AprilTagLookup.get(tagID);
+      Transform3d fiducialTransform = new Transform3d(tagPose.getTranslation(), tagPose.getRotation());;
+      Transform3d targetToCamera = target.getBestCameraToTarget().inverse(); // Take inverse to find "targetToCamera"
+      Transform3d cameraToRobot = VISIONCONSTANTS.CAMERA_POS;
+
+      bestPose = Vision.getPoseFromTarget(tagPose, fiducialTransform, targetToCamera, cameraToRobot);
       break;
     }
 
-    if (bestTarget == null) return null;
+    return bestPose;
+  }
 
-    // Get Pose3d from target
-    int tagID = bestTarget.getFiducialId();
-
-    Pose3d tagPose = AprilTagLookup.get(tagID);
-    Transform3d fiducialTransform = new Transform3d(tagPose.getTranslation(), tagPose.getRotation());;
-    Transform3d targetToCamera = bestTarget.getBestCameraToTarget().inverse(); // Take inverse to find "targetToCamera"
-    Transform3d cameraToRobot = VISIONCONSTANTS.CAMERA_POS;
-
+  /** */
+  public static final Pose3d getPoseFromTarget(
+    Pose3d tagPose,
+    Transform3d fiducialTransform,
+    Transform3d targetToCamera,
+    Transform3d cameraToRobot
+  ) {
     Transform3d robotTransform = fiducialTransform.plus(targetToCamera).plus(cameraToRobot);
 
     return new Pose3d(
